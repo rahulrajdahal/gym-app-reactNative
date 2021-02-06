47461334fd6cc3acd3bdfc949639d1117f2d81a6
Markaa10 <rahulrajdahal@gmail.com>
2021-02-06 22:28:39 +0545

    elevation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import { COLORS, FONTS, SIZES } from "../constants";
-import { Gift } from "../constants/icons";
+import { Gift, Flag } from "../constants/icons";
 import { Profile } from "../constants/images";
 
 const Home = () => {
@@ -107,16 +107,155 @@ const Home = () => {
     );
   }
 
+  function renderActivity() {
+    return (
+      <View
+        style={{
+          marginTop: 35,
+          marginLeft: 27,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          // justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Flag style={{ color: "rgba(255, 73, 58, 0.2)" }} />
+          <Text style={{ color: "#656565", ...FONTS.body1, marginLeft: 7.8 }}>
+            You’re doing great so far:
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: 21,
+            width: 374,
+            height: 111,
+            paddingRight: 15,
+            paddingLeft: 24,
+            paddingTop: 13,
+            paddingBottom: 8,
+            shadowRadius: 15,
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: 365,
+              height: 106,
+              backgroundColor: "#F0EBEB",
+              borderRadius: SIZES.boderRadius,
+              paddingTop: 11,
+              paddingBottom: 40,
+              paddingLeft: 10,
+              paddingRight: 73,
+              elevation: 5,
+            }}
+          >
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: 66,
+                height: 55,
+              }}
+            >
+              <Text
+                style={{ color: "#262626", ...FONTS.body1, fontWeight: "300" }}
+              >
+                Average
+              </Text>
+              <Text
+                style={{
+                  color: "rgba(36, 161, 156, 0.4)",
+                  ...FONTS.h1,
+                  lineHeight: 40,
+                  fontWeight: "bold",
+                }}
+              >
+                8.0
+              </Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: 66,
+                height: 55,
+              }}
+            >
+              <Text
+                style={{ color: "#262626", ...FONTS.body1, fontWeight: "300" }}
+              >
+                Goal
+              </Text>
+              <Text
+                style={{
+                  color: "rgba(36, 161, 156, 0.4)",
+                  ...FONTS.h1,
+                  lineHeight: 40,
+                  fontWeight: "bold",
+                }}
+              >
+                8
+              </Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: 66,
+                height: 55,
+              }}
+            >
+              <Text
+                style={{ color: "#262626", ...FONTS.body1, fontWeight: "300" }}
+              >
+                Total
+              </Text>
+              <Text
+                style={{
+                  color: "rgba(36, 161, 156, 0.4)",
+                  ...FONTS.h1,
+                  lineHeight: 40,
+                  fontWeight: "bold",
+                }}
+              >
+                200
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView>
       <View style={{ flex: 1, backgroundColor: COLORS.white }}>
-        <Text>Home</Text>
-
         {/* Welcome Header */}
         {renderWelcomeHeader()}
 
         {/* Progress Indicator And Reward */}
         {renderProgress()}
+
+        {/* Activity  */}
+        {renderActivity()}
       </View>
     </SafeAreaView>
   );
