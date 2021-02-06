e41d54ca2101fd2e53f227f0ee876be7ac79322f
Markaa10 <rahulrajdahal@gmail.com>
2021-02-06 21:51:47 +0545

    progressIndicator

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
-import { COLORS, FONTS } from "../constants";
+import { COLORS, FONTS, SIZES } from "../constants";
+import { Gift } from "../constants/icons";
 import { Profile } from "../constants/images";
 
 const Home = () => {
@@ -36,6 +37,76 @@ const Home = () => {
     );
   }
 
+  function renderProgress() {
+    return (
+      <View
+        style={{
+          width: 295,
+          height: 84,
+          marginTop: 84,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignSelf: "center",
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: 254,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: COLORS.primary,
+              opacity: 0.2,
+              width: 58,
+              height: 58,
+              borderRadius: SIZES.boderRadius,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Gift style={{ color: COLORS.primary, opacity: 1 }} />
+          </View>
+          <Text
+            style={{
+              marginLeft: 12,
+              marginRight: 41,
+              color: "#9A9A9A",
+              ...FONTS.body1,
+              fontWeight: "500",
+              width: 184,
+            }}
+          >
+            You’re 23 hours away from your reward
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: 16,
+            width: 295,
+            height: 10,
+            borderRadius: 9.5,
+            backgroundColor: "#E7E7E7",
+          }}
+        >
+          <View
+            style={{
+              width: 212,
+              height: 10,
+              borderRadius: 9.5,
+              backgroundColor: COLORS.primary,
+            }}
+          ></View>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView>
       <View style={{ flex: 1, backgroundColor: COLORS.white }}>
@@ -43,6 +114,9 @@ const Home = () => {
 
         {/* Welcome Header */}
         {renderWelcomeHeader()}
+
+        {/* Progress Indicator And Reward */}
+        {renderProgress()}
       </View>
     </SafeAreaView>
   );
